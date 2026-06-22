f8253575514b2947dfec924e0890d233552c5bba
PouffyDev <99536749+pouffy@users.noreply.github.com>
2026-06-22 21:32:51 +0100

    Fix render bounding box for grand door

--- src/main/java/com/benji/netherman/client/renderer/GrandDoorRenderer.java
+++ src/main/java/com/benji/netherman/client/renderer/GrandDoorRenderer.java
@@ -6,6 +6,7 @@ import com.benji.netherman.client.model.GrandDoorModel;
 import com.benji.netherman.block.entity.GrandDoorBlockEntity;
 import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.phys.AABB;
 import software.bernie.geckolib.renderer.GeoBlockRenderer;
 
 public class GrandDoorRenderer extends GeoBlockRenderer<GrandDoorBlockEntity> {
@@ -14,5 +15,10 @@ public class GrandDoorRenderer extends GeoBlockRenderer<GrandDoorBlockEntity> {
 
         
         addRenderLayer(new GenericEmissiveLayer<>(this, ResourceLocation.fromNamespaceAndPath(NetherExp.MODID, "textures/block/grand_door_emissive.png")));
+    }
+
+    @Override
+    public AABB getRenderBoundingBox(GrandDoorBlockEntity blockEntity) {
+        return new AABB(blockEntity.getBlockPos()).inflate(3, 12, 3);
     }
 }
